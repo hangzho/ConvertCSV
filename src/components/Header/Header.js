450e331b333dcf0c4898096465efe4769452c11b
Hang Zhou <hzhou@signetaccel.com>
2017-01-29 14:16:55 -0500

    Create the convertCsv route.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,20 @@ import { IndexLink, Link } from 'react-router';
 import './Header.scss';
 
 export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
+    <div >
+        <h1>React Redux Starter Kit</h1>
+        <IndexLink to='/' className='btn btn-default' activeClassName='btn-primary'>
+            Home
+        </IndexLink>
+        {' · '}
+        <Link to='/counter' className='btn btn-default' activeClassName='btn-primary'>
+            Counter
+        </Link>
+        {' · '}
+        <Link to='/convertCsv' className='btn btn-default' activeClassName='btn-primary'>
+            Convert CSV
+        </Link>
+    </div>
 );
 
 export default Header;
